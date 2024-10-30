152f437795308719150ad6afad9ceb18313face2
daotq1 <v.daotq1@vinhomes.vn>
2024-10-30 13:55:49 +0700

    update

---
+++ src/utils/Constaints.js
@@ -0,0 +1,11 @@
+export const API_ENDPOINT = 'https://be10-101-99-23-84.ngrok-free.app/'
+
+export const STATUS_CODE = {
+    SUCCESS: 200,
+    CREATED: 201,
+    UPDATED: 202,
+    UNAUTHORIZED: 401,
+    ACCEPTED: 403,
+    NOT_ACCEPTABLE:406,
+    FAILED: 500
+}
