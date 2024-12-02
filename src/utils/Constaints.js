6356def8d3436299a5d376d307483924b0adf855
daotq1 <v.daotq1@vinhomes.vn>
2024-12-02 18:26:12 +0700

    update endpoint

--- src/utils/Constaints.js
+++ src/utils/Constaints.js
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = 'https://f79f-34-57-56-213.ngrok-free.app/'
+export const API_ENDPOINT = 'https://fc54-34-57-56-213.ngrok-free.app/'
 
 export const STATUS_CODE = {
     SUCCESS: 200,
